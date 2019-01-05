20e00b9d291593b95c7a10ed3cd4562cec920ff1
Svetlana <stojadinov.svetlana@gmail.com>
2019-01-05 14:33:25 +0100

    request bank-pcc

--- bank/src/main/java/com/ftn/uns/bank/controller/TransactionController.java
+++ bank/src/main/java/com/ftn/uns/bank/controller/TransactionController.java
@@ -2,19 +2,25 @@ package com.ftn.uns.bank.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.ftn.uns.bank.model.ClientAccount;
 import com.ftn.uns.bank.model.PaymentCallback;
@@ -33,6 +39,7 @@ public class TransactionController {
 	private PaymentSameBank paymentSameBank;
 
 	private String bankCode = "1";
+	private String urlPcc="http://localhost:8082/api/banks/payment";
 
 	private String url = "http://localhost:1337";
 
@@ -82,6 +89,15 @@ public class TransactionController {
 			response = paymentSameBank.completeTransaction(clientAccount, paymentId);
 		} else {
 			// pcc
+			Map<String, Object> request = new HashMap<String, Object>();
+			request.put("ACQUIRER_ORDER_ID",(long)(Math.random()*(9999999999.00 - 1000000000 + 1) + 1000000000));
+			request.put("ACQUIRER_TIMESTAMP", new Date());
+			request.put("clientAccount", clientAccount);
+			
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			ResponseEntity<Map> responseEntity = new RestTemplate().exchange(urlPcc, HttpMethod.POST,
+					new HttpEntity<Map>(request, headers), Map.class);
 		}
 
 		return response;
